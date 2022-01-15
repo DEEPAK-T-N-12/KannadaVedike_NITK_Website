10d88ebdb2fb6ad225980096c73a94bcb80e6b20
Saikiran Patil <patilrsaikiran@gmail.com>
2022-01-15 20:08:54 +0530

    added events card

--- index.js
+++ index.js
@@ -33,3 +33,55 @@ $(document).ready(function () {
 })
 
 // translator button function ends here 
+
+header.innerHTML=`
+                    <header class="flex px-3">
+                    <div class="languageSection flex">
+                    <img class="m-auto" src="/icons/language.svg" width="20" height="20" alt="">
+                    <div id="language" class="m-auto pl-1" onclick="translate_full_page()" role="button">
+                        ಕನ್ನಡ
+                    </div>
+                    </div>
+                    <div class="support mr-5 py-2 px-4 red-bg white-txt">
+                    <a href="#">
+                        Support Us
+                    </a>
+                    </div>
+                    </header>
+                    <nav class="navbar navbar-expand-lg navbar-dark bg-dark navbar-custom ">
+                    <a class="navbar-brand" href="#">
+                    <img id="logo" src="/images/logo.png" width="100" height="100" alt="">
+                    </a>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                    aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                    </button>
+
+                    <div class="collapse navbar-collapse ml-auto" id="navbarSupportedContent">
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item active">
+                        <a class="nav-link" href="/index.html">home</a>
+                        </li>
+                        <li class="nav-item">
+                        <a class="nav-link" href="#about">about us</a>
+                        </li>
+                        <li class="nav-item">
+                        <a class="nav-link" href="events.html">events</a>
+                        </li>
+                        <li class="nav-item">
+                        <a class="nav-link" href="#">social activities</a>
+                        </li>
+                        <li class="nav-item">
+                        <a class="nav-link" href="#">alumni</a>
+                        </li>
+                        <li class="nav-item">
+                        <a class="nav-link" href="#">contact us</a>
+                        </li>
+                        <li class="nav-item">
+                        </li>
+                    </ul>
+                    </div>
+                    </nav>
+`;
+
+footer.innerHTML=``;
